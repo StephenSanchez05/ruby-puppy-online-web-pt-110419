4b364a240e4dab903fa475baa52e6ccfe1c652e0
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-08 20:04:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,31 +2,26 @@ class Dog
 
 @@all = []
 
-@@dog_name = []
 
 attr_accessor :name 
 
 def initialize(name)
   @name = name
   save
-  unless @@dog_name.include?(name)
-  @@dog_name << name
   end
-end
 
   def self.all
     @@all
   end
   
   def self.print_all
-    print @@dog_name.join(", ")
+    print @@all.map{ |dog| dog.name}
   end
   
   def self.clear_all
     @@all.clear 
   end
   
-  private 
     def save
     @@all << self
   end
